1eace33d47487f6c629730058077909b83f4513f
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-05-20 12:10:23 -0400

    trying to fix the addTransaction function

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -15,9 +15,26 @@ export const GlobalContext = createContext(initialState)
 export const GlobalProvider = ({children}) => {
     const [state, disptach] = useReducer(AppReducer, initialState)
 
+    function deleteTransaction(id) {
+        disptach({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+
+    }
+
+    function addTransaction(transaction) {
+        disptach({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
     return (
     <GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>
